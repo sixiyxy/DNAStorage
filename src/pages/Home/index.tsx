2a173b65d3e029599faf5b302ac11d9e1e77fddb
sixiyxy <771702500@qq.com>
2022-07-10 21:31:13 +0800

    首页添加upload组件用于联调测试

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,45 @@
 import React from "react";
 import { PathRouteProps } from "react-router-dom";
 import "./index.less";
+import { InboxOutlined } from "@ant-design/icons";
+import type { UploadProps } from "antd";
+import { message, Upload } from "antd";
 export class HomeProps {}
 
+const { Dragger } = Upload;
+const props: UploadProps = {
+  name: "file",
+  multiple: true,
+  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log("Dropped files", e.dataTransfer.files);
+  },
+};
 export const Home: React.FC<HomeProps> = (props) => {
-  return <div> Home content</div>;
+  return (
+    <Dragger {...props}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading
+        company data or other band files
+      </p>
+    </Dragger>
+  );
 };
 Home.defaultProps = new HomeProps();
