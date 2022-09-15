6f4ca4630a47692caf303be0ba135b8bdfd3fb01
zoezzy0125 <zoezzy0125@gmail.com>
2022-09-15 19:38:57 +0800

    arg update

--- backend/script/utils/simulation_arg.py
+++ backend/script/utils/simulation_arg.py
@@ -362,4 +362,33 @@ seqMeth={
             "seq_ins_pos":{"homopolymer":0.85,"random":0.15},
             "TM_Normal":False
         },
+
+        "Pac_subread":{
+            "seq_sub_prob":0.75,
+            "seq_ins_prob":0.05,
+            "seq_del_prob":0.2,
+            "seq_raw_rate":0.02,
+            "seq_del_pattern":{"A":0.15,"C":0.35,"G":0.35,"T":0.15},
+            "seq_ins_pattern":{"A":0.35,"C":0.15,"G":0.15,"T":0.35},
+            "seq_sub_pattern":{
+                                "CG":{"CA":0.5,"TG":0.5}
+                                },
+            "seq_del_pos":{"homopolymer":0.85,"random":0.15},
+            "seq_ins_pos":{"homopolymer":0.85,"random":0.15},
+            "TM_Normal":False
+        },
+        "Pac_CCS":{
+            "seq_sub_prob":0.37,
+            "seq_ins_prob":0.42,
+            "seq_del_prob":0.21,
+            "seq_raw_rate":0.14,
+            "seq_del_pattern":{"A":0.15,"C":0.35,"G":0.35,"T":0.15},
+            "seq_ins_pattern":{"A":0.35,"C":0.15,"G":0.15,"T":0.35},
+            "seq_sub_pattern":{
+                                "CG":{"CA":0.5,"TG":0.5}
+                                },
+            "seq_del_pos":{"homopolymer":0.85,"random":0.15},
+            "seq_ins_pos":{"homopolymer":0.85,"random":0.15},
+            "TM_Normal":False
+        },
 }
