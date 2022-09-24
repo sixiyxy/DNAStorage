ec6f5ee059c7242488e2abb359cec67b1df1e9c5
sixiyxy <771702500@qq.com>
2022-09-24 15:57:09 +0800

    fix:build

--- src/pages/Services/Encode/index.jsx
+++ src/pages/Services/Encode/index.jsx
@@ -1,45 +1,43 @@
-import React,{useState,useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import "./index.less";
-import { Breadcrumb } from 'antd';
-import {Encodelist} from './components/Encodelist'
-import Encodelists from './components/Encodelists'
-import Uploads from './components/Uploads'
-import Sliders from './components/Sliders'
-import Graphs from './components/Graphs'
+import { Breadcrumb } from "antd";
+import { Encodelist } from "./components/Encodelist";
+import Encodelists from "./components/Encodelists";
+import Uploads from "./components/Uploads";
+import Sliders from "./components/Sliders";
+import Graphs from "./components/Graphs";
 
-import { Anchor } from 'antd';
+import { Anchor } from "antd";
 const { Link } = Anchor;
 
 //export class EncodeProps {}
-var fileID = []
+var fileID = [];
 
-export const Encode= (props) => {
-
+export const Encode = (props) => {
   const [targetOffset, setTargetOffset] = useState(undefined);
   useEffect(() => {
     setTargetOffset(window.innerHeight / 2);
   }, []);
 
-  const SaveValue = [{Segvalue:0},{Indexvalue:0}]
+  const SaveValue = [{ Segvalue: 0 }, { Indexvalue: 0 }];
   //const FileValue = [{fileuid:''},{filename:''},{filerename:''},{filetype:''}]
-  const [values,setValues] = useState(SaveValue);
-  const [method,setMethod] = useState('WithoutVerifycode')
+  const [values, setValues] = useState(SaveValue);
+  const [method, setMethod] = useState("WithoutVerifycode");
   //const [fileID,setfileID] = useState([])
-  
 
   //获取Slider中的参数信息传递给Graph
-  const ParamPass=(param1,param2)=>{
-    setValues(param2) //value
-    setMethod(param1) //method
-    console.log("父亲",values);
-    console.log("父亲",method);
-  }
+  const ParamPass = (param1, param2) => {
+    setValues(param2); //value
+    setMethod(param1); //method
+    console.log("父亲", values);
+    console.log("父亲", method);
+  };
   //获取fileID — — — —子传父的回调
-  const GetFileID = (fileobj)=>{
+  const GetFileID = (fileobj) => {
     //setfileID(fileobj);
-    fileID =  fileobj
-    console.log('父亲',fileID);
-  }
+    fileID = fileobj;
+    console.log("父亲", fileID);
+  };
   //获取解码方法 — — — — 回调
 
   return (
@@ -55,35 +53,46 @@ export const Encode= (props) => {
             <Breadcrumb.Item>Encode Data</Breadcrumb.Item>
         </Breadcrumb>
       </div> */}
-      <div id='uploads' style={{paddingLeft:"250px",paddingTop:"20px",fontSize:"18px"}}>
-        <p ><strong>Please upload the storage files:</strong></p> 
-        <Uploads GetFileID={GetFileID}/>
+      <div
+        id="uploads"
+        style={{ paddingLeft: "250px", paddingTop: "20px", fontSize: "18px" }}
+      >
+        <p>
+          <strong>Please upload the storage files:</strong>
+        </p>
+        <Uploads GetFileID={GetFileID} />
       </div>
-      <div id='sliders' style={{paddingLeft:"250px",paddingTop:"50px",fontSize:"14px"}}>
-        <hr/>
-        <Sliders ParamPass={ParamPass}/>
+      <div
+        id="sliders"
+        style={{ paddingLeft: "250px", paddingTop: "50px", fontSize: "14px" }}
+      >
+        <hr />
+        <Sliders ParamPass={ParamPass} />
       </div>
-      <div id='graphs' style={{paddingLeft:"250px",paddingTop:"20px"}}>
-        <hr/>
+      <div id="graphs" style={{ paddingLeft: "250px", paddingTop: "20px" }}>
+        <hr />
         <Graphs values={values} method={method} />
       </div>
-      
-      <div id='encodelist' style={{paddingLeft:"150px",paddingTop:"20px",fontSize:"15px"}}>
-        <p style={{paddingLeft:"100px",fontSize:"17px"}}><hr/><strong>请在下面的勾选框中选出适合自己的编码方法:</strong></p>
-        <Encodelists fileID={fileID} values={values} method={method}/>
-      </div>
-      <div style={{ position:"fixed",top:"200px",margin:"0px 1150px"}}>
-            <Anchor targetOffset={targetOffset}>
 
-              <Link href="#uploads" title="File Upload" />
-              <Link href="#sliders" title="Choose method and length" />
-              <Link href="#graphs" title="Draw" />
-              <Link href="#encodelist" title="Choose decode method" />
-      
-            </Anchor>  
-        </div>
+      <div
+        id="encodelist"
+        style={{ paddingLeft: "150px", paddingTop: "20px", fontSize: "15px" }}
+      >
+        <p style={{ paddingLeft: "100px", fontSize: "17px" }}>
+          <hr />
+          <strong>请在下面的勾选框中选出适合自己的编码方法:</strong>
+        </p>
+        <Encodelists fileID={fileID} values={values} method={method} />
+      </div>
+      <div style={{ position: "fixed", top: "200px", margin: "0px 1150px" }}>
+        <Anchor targetOffset={targetOffset}>
+          <Link href="#uploads" title="File Upload" />
+          <Link href="#sliders" title="Choose method and length" />
+          <Link href="#graphs" title="Draw" />
+          <Link href="#encodelist" title="Choose decode method" />
+        </Anchor>
+      </div>
     </div>
-    
   );
 };
 //Encode.defaultProps = new EncodeProps();
