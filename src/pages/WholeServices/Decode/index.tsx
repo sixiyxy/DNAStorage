5d7266166b4fd356217618e06c4b51b22671cb55
sixiyxy <771702500@qq.com>
2022-11-17 17:03:53 +0800

    feat:decode

--- src/pages/WholeServices/Decode/index.tsx
+++ src/pages/WholeServices/Decode/index.tsx
@@ -10,6 +10,7 @@ import {
 import axios from "axios";
 import React, { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
+import { Image } from 'antd';
 import "./index.less";
 import {API_PREFIX} from "../../../common/Config";
 export class DecodeProps {
@@ -28,6 +29,7 @@ export const Decode: React.FC<DecodeProps> = (props) => {
       title: "Name",
       dataIndex: "name",
       key: "name",
+      width: 400,
     },
     {
       title: "Value",
@@ -40,64 +42,55 @@ export const Decode: React.FC<DecodeProps> = (props) => {
     return [
       {
         key: "1",
-        name: "decode_time",
+        name: "Decode time",
         value: data?.decode_time || "",
       },
       {
         key: "2",
-        name: "clust_method",
+        name: "Clust time",
         value: data?.clust_method || "",
       },
       {
         key: "3",
-        name: "encode_dna_sequence_number",
+        name: "Encode DNA sequence number",
         value: data?.encode_dna_sequence_number || "",
       },
       {
         key: "4",
-        name: "after_clust_dna_sequence_number",
-        value: data?.after_clust_dna_sequence_number || "",
+        name: "Simulation DNA sequence number",
+        value: data?.encode_dna_sequence_number || "",
       },
       {
         key: "5",
-        name: "recall_dna_sequence_number",
-        value: data?.recall_dna_sequence_number || "",
+        name: "Cluster DNA sequence number",
+        value: data?.after_clust_dna_sequence_number || "",
       },
       {
         key: "6",
-        name: "recall_dna_sequence_rate",
-        value: data?.recall_dna_sequence_rate || "",
+        name: "Recall DNA sequence number",
+        value: data?.recall_dna_sequence_number || "",
       },
       {
         key: "7",
-        name: "verify_method_remove_bits",
-        value: data?.verify_method_remove_bits || "",
+        name: "Recall rate",
+        value: data?.recall_dna_sequence_rate || "",
       },
       {
         key: "8",
-        name: "encode_bits_number",
-        value: data?.encode_bits_number || "",
+        name: "Encode bits fragments",
+        value: data?.verify_method_remove_bits || "",
       },
       {
         key: "9",
-        name: "final_decode_bits_number",
-        value: data?.final_decode_bits_number || "",
+        name: "Decode bits fragments",
+        value: data?.encode_bits_number || "",
       },
       {
         key: "10",
-        name: "recall_bits_number",
-        value: data?.recall_bits_number || "",
+        name: "Recall bits fragments",
+        value: data?.final_decode_bits_number || "",
       },
-      {
-        key: "11",
-        name: "error_bits_number",
-        value: data?.error_bits_number || "",
-      },
-      {
-        key: "12",
-        name: "error_bits_rate",
-        value: data?.error_bits_rate || "",
-      },
+
     ];
   }, [data]);
 
@@ -130,33 +123,53 @@ export const Decode: React.FC<DecodeProps> = (props) => {
           <Breadcrumb.Item>Decode</Breadcrumb.Item>
         </Breadcrumb>
       </div>
-      <Card style={{ margin: "10px 30px", width: 600 }}>
-        <h2>DNA Sequence Cluster</h2>
-        <div style={{ paddingLeft: "20px" }}>
+      <div
+              style={{ marginTop: "30px", marginLeft: "120px",width:"100%"}} className="FileInformation"
+            >
+              <Card title="Summary" style={{width:"90%"}} headStyle={{backgroundColor:'#99CCFF'}} >
+                <div style={{display:"flex"}}>
+                  <div>
+                  <p style={{
+                    margin: "0px 10px 20px 10px",
+                    fontSize: "15px",
+                    width: "400px",
+                  }}>In the DNA storage process, file recovery needs to decode the DNA sequences according to the reverse process of the encoding roles.
+                  However，DNA sequences obtained by sequencing usually have random errors(insert\indel\SNV, we have simulated this).
+                  So, we designed two clustering algorithms, CD-HIT and Starcode, to remove de-redundancy and correct the sequencing data.
+                  Then, the clustered sequences will decode according to the rules to obtain bits fragments. Subsequently,
+                  the bits fragments will be removed from the verification code and index code. Finally, we analyze the recovery information of bits fragment in the report.</p>
+                  <Button
+                type="primary"
+                shape="round"
+                size={"large"}
+                style={{width:"100px", margin:"5px 20px"}}
+              >
+                Example
+              </Button>
+                  </div>
+                  <Image src="/src/assets/service/decode.png" />
+                </div>
+              </Card>
+              <Card title="Cluster algorithms" style={{width:"90%"}} headStyle={{backgroundColor:'#99CCFF'}} >
+                <div style={{ paddingLeft: "20px" }}>
           <Radio.Group onChange={onChange} value={value}>
             <Space direction="vertical">
-              <Radio value={"cdhit"}>Cdhit</Radio>
-              <Radio value={"starcode"}>xxx</Radio>
+              <Radio value={"cdhit"}><strong>CD-HIT: </strong>a fast program for clustering and comparing large sets of protein or nucleotide sequences. Bioinformatics 22.13 (2006): 1658-1659. </Radio>
+              <Radio value={"starcode"}><strong>Starcode: </strong>sequence clustering based on all-pairs search. Bioinformatics 31.12 (2015): 1913-1919. </Radio>
             </Space>
           </Radio.Group>
         </div>
-        <div>
-          <Button
-            type="primary"
-            style={{ margin: "30px 20px 0" }}
-            onClick={onDecode}
-          >
-            Run
-          </Button>
-        </div>
-      </Card>
-      <div style={{ marginTop: "30px", marginLeft: "30px" }}>
+              </Card>
+              <Card title="Result" style={{width:"90%"}} headStyle={{backgroundColor:'#99CCFF'}} >
+              <div style={{ marginTop: "5px", marginLeft: "30px" }}>
         <Table
           columns={columns}
           dataSource={tableData}
           pagination={{ position: ["none"] }}
         />
       </div>
+              </Card>
+            </div>
     </div>
   );
 };
