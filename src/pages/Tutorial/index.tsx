ef126ca05bcbc399ce177348147e18afe366e776
zoezzy0125 <zoezzy0125@gmail.com>
2022-11-28 19:27:33 +0800

    tut minor change

--- src/pages/Tutorial/index.tsx
+++ src/pages/Tutorial/index.tsx
@@ -11,7 +11,9 @@ import simubtn from "../../assets/tutorial/simubtn.png"
 import SYN from "../../assets/tutorial/SYN.png"
 import simutab from "../../assets/tutorial/simutab.png"
 import simuseq from "../../assets/tutorial/simuseq.png"
+import copynumber from "../../assets/tutorial/copynumber.png"
 import simucounts from "../../assets/tutorial/simucounts.png"
+import dnatable from "../../assets/tutorial/dnatable.png"
 import type { ColumnsType } from "antd/es/table";
 export class TutorialProps {}
 
@@ -206,14 +208,14 @@ export const Tutorial: React.FC<TutorialProps> = (props) => {
             With the total amount of worldwide data skyrocketing, traditional storage methods face daunting challenges. 
             The international data storage demand will far exceed the storage capacity of any currently available storage method. 
             DNA molecules as information carriers have many advantages over traditional storage media. Its high storage density, 
-            potentially low maintenance cost and other excellent characteristics make it an ideal alternative for information storage, 
+            potentially low maintenance cost and other excellent characteristics make it an ideal alternative medium for information storage, 
             and is expected to provide wide practicality in the future. DNA Storage Designer is an easy-to-use online web server that covers 
             the simulation of the whole process of DNA storage application. It includes 8 popular encoding methods and realized the corresponding decoding process. 
-            In addition, it also simulated the sequence errors in the five DNA sequence storage processes of synthesis, decay, PCR, sample, and sequencing. 
+            In addition, it also simulates the sequence errors in the five DNA sequence storage processes of synthesis, decay, PCR, sample, and sequencing. 
             Users can not only use independent encode modules to encode files and obtain DNA sequences for vivo/in vivo storage experiments, 
-            but also upload DNA sequences to simulate sequence errors during storage by the 'Error Simulation' modules. 
-            Aiming to provide user-friendly services, this website embedded the most popular methods and platforms related to DNA storage instead of asking users for complex parameters. 
-            It also gives thorough guidelines and simulated feedback based on user settings so that user could adjust their experimental plan based on the report.
+            but also upload DNA sequences to simulate sequence errors during storage by the 'Error Simulation' module. 
+            Aiming to provide user-friendly services, this website embeds the most popular methods and platforms related to DNA storage instead of asking users for complex parameters. 
+            It also gives thorough guidelines and simulated feedback based on user settings so that user could adjust their experimental plan according to the feedback report.
             </p>
           </div>
           <div id="example-file">
@@ -222,7 +224,7 @@ export const Tutorial: React.FC<TutorialProps> = (props) => {
               We use the Xiamen University badge picture as an example file to introduce the working process of the website.
               </p>
               <Image src={xiamen} width={"50%"} rootClassName="image2"/>
-              <p style={{textAlign:"center"}}>Example File</p>
+              <p style={{textAlign:"center",fontSize:13}}>Figure 1: Example file</p>
               <p id="text-content">
               Other demo files can be downloaded from here. The file types include: Image, video, audio, text, and binary.
 
@@ -240,7 +242,7 @@ export const Tutorial: React.FC<TutorialProps> = (props) => {
               <li><strong>Upload the storage file: </strong>You can use the demo file or your own files, but the file size is limited to less than 10M (the encoding time for large files is too long).</li>
                 <li><strong>Encode Method: </strong>You can choose one of the existing popular encoding methods to encode the file, but <i>Zan's</i> code can only encode the English letters in the txt file. For more details on each method, please refers to the <a href="/methods#/methods">Method</a>. </li>
                 <li><strong>Verify code: </strong>Because the DNA sequence will inevitably have errors during the storage process, in order to ensure the accuracy of data, we integrated two <strong>verify codes</strong>, which is <i>Reed Solomon Code and Hamming Code.</i></li>
-                <li><strong>Segment length and Index length: </strong>The upload file will be compiled into binary data, it will be split into fixed lengths and converted to DNA sequence using the chosen method, this fixed length is the segment length. Since the existing synthesis and sequencing technology do not support long sequences, the segment length is less than 200. At the same time, in order to restore the data, each segment will add address information, and its length is the index length.</li>
+                <li><strong>Segment length and Index length: </strong>The upload file will be compiled into binary data, it will be split into fixed lengths and converted to DNA sequence using the chosen method, this fixed length is the segment length. Since the existing synthesis and sequencing technology do not support long sequences, the segment length should be less than 200. At the same time, in order to restore the data, each segment will be added on address information, and the length is the index length.</li>
                 <li><strong>Run: </strong>Encode the upload file with the chosen setting parameters.</li>
               </ul>
             </div>
@@ -257,13 +259,16 @@ export const Tutorial: React.FC<TutorialProps> = (props) => {
                   />
                   <p id="report-content">The analysis of the encoded DNA sequences includes GC-content statics, Repeated sequence statics, and minimum free energy calculation. Here, we randomly sampled only 1000 DNA sequences for analysis.</p>
                   <Image src={gc} width={"50%"} rootClassName="image3"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 2: GC content diagram</p>
                   <p id="report-content">Because the ratio of GC content is the crucial indicator of the stability of DNA sequence, we counted the GC content of each encoded DNA sequence. The X-axis is the percentage of GC content, and the Y-axis is the number of corresponding sequences.</p>
                   <Image src={rp} width={"50%"} rootClassName="image4"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 3: Homopolymer diagram</p>
                   <p id="report-content">The presence of repetitive sequences affects the accuracy of synthesis and sequence sequencing during DNA storage. So, we counted the number of repeats in the encoded DNA sequence. The X-axis is the length of the repeated sequence, and the Y-axis is the corresponding number.</p>
                   <Image src={free} width={"50%"} rootClassName="image5"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 4: Minimum free energy diagram</p>
                   <p id="report-content"><strong>The sequence average minimum free energy is: -5.82</strong><br></br>
                   The minimum free energy (MFE) of a DNA sequence is the minimum of the Gibbs standard free energy of all possible secondary structures. Therefore, the quality of DNA sequences can be measured by calculating the MFE of each sequence. Here we calculated the minimum free energies if randomly selected 1000 encoded DNA sequences by RNAfold(see methods).<br></br><br></br>
-                  Here we use the RNAfold command is:<br></br>
+                  Here we use the RNAfold command as following:<br></br>
                   <code className="code1">{'{RNAfold} --noPS --noGU --noconv -T 59.1 < {encode dna sequence file} > {outfile}'}</code>
                   </p>
                   <li><strong>Download: </strong>Include two files in the downloaded compressed file, one is the encoding settings information of the uploaded file(named as file_id.yaml), and the other is the encoded DNA sequence file (including: payload, index, index_payload, index_payload_verfiycode, DNA_sequence. namely file_id.csv).</li>
@@ -277,21 +282,36 @@ export const Tutorial: React.FC<TutorialProps> = (props) => {
                   <li><strong>Example: </strong>An example is embedded in this part based on the previous encoding example, and all the simulation settings are using the default ones.</li>
                   <li><strong>Stages Setting: </strong>We list out five processes, namely, synthesis, decay(storage), PCR, sampling, and sequencing. You could decide whether these stages are needed or not to get a final workflow based on your experiment design. By default, all stages are simulated. If you want to skip some of them, you could click on the corresponding buttons. However, because the synthesis of sequences is the base of the following stages, this stage is a must for the simulation part and can not be ignored. After your decision is done, please click the “ok” button to start your detailed settings.</li>
                   <Image src={simubtn} width={"80%"} rootClassName="image6"/>
-                  <li><strong>Detail Settings: </strong>Because the errors that occur in stages have accumulative effects, the execution order of stages is fixed. You should set the stages accordingly. For each parameter, you could hover your mouse over the question mark aside, the explanation will be popped out. As for the settings with provided options, you could see their corresponding short introduction and references from the <a href="/methods#/methods">Method</a> page. After setting up parameters for one stage, please click on the “ok” button to confirm and the sequence distribution changes result of this stage will be shown instantly right. Then, you could move to the next stage. </li>
-                </ul>
-                <p id='simuset-report'>After all the stages are done and the diagrams are shown, please click on the report button to get the overall result and summary. </p>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 5: Simulation stage settings</p>
+                  <li><strong>Detail Settings: </strong>Because the errors that occur in stages have accumulative effects, the execution order of stages is fixed. You should set the stages accordingly. For each parameter, you could hover your mouse over the question mark aside, the explanation will be popped out. As for the settings with provided options, you could see their corresponding short introduction and references from the “Methods” page. After setting up parameters for one stage, please click on the “ok” button to confirm. </li>
                   <Image src={SYN} width={"40%"} rootClassName="image6"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 6: Sample of hovering information</p>
+                </ul>
+                <p id='simuset-report'>And the sequence distribution changes result of this stage will be shown instantly right. This diagram shows the sequence number distribution of oligonucleotides pool, which explains the density of DNA strands. </p>
+                <Image src={copynumber} width={"40%"} rootClassName="image6"/> 
+                <p style={{textAlign:"center",fontSize:13}}>Figure 7: Sequence distribution diagram</p>
+                <p id='simuset-report'>To be specific, for example, if we have an original sequence<i> M </i>at first, after 10 cycle synthesis, we may get 10 copies for<i>M</i>, among them, some may be embedded with simulated errors (<i>M<sub>1</sub></i>,<i>M<sub>2</sub></i>).</p>
+                <Image src={dnatable} width={"80%"} rootClassName="image6"/> 
+                <p style={{textAlign:"center",fontSize:13}}>Figure 8: Example of simulated situation of <i>M</i></p>
+                <p id='simuset-report'>Thus, for this example, in this stage, most DNA strands have a copy number around 10, while strands with 1 or 2 copy number also exist.Therefore, the diagrams will help you to intuitively understand the density change of chains in nucleic acid pool. Then, you could move to the next stage.</p>
+                <p id='simuset-report'>After all the stages are done and the diagrams are shown, please click on the report button to get the overall result and summary. </p>
+                
               </div>
               <div id="simu-report">
                 <h3 id="seventh-title">2.2 Report</h3>
-                <p id='text-content'>The report consists of three parts, Settings Report, Sequences Distribution, and Error Counts. You could also download the simulated file together with the setting information using the “Download” button. </p>
+                <p id='text-content'>The report consists of three parts, Stage Summary, Sequences Distribution, and Error Counts. You could also download the simulated file together with the setting information using the “Download” button. </p>
                 <ul>
-                  <li><strong>Settings Report: </strong>This part lists out the choices you made in the setting part. Also, it uses pie charts to uncover the distribution of different error types of corresponding chosen methods. You could click on the stages and double-check previous settings.</li>
+                  <li><strong>Stage Summary: </strong>This part lists out the choices you made in the setting part. Also, it uses pie charts to uncover the distribution of different error types of corresponding chosen methods. You could click on the stages and double-check previous settings.</li>
                   <Image src={simutab} width={"75%"} rootClassName="image6"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 9: Example of stage summary</p>
                   <li><strong>Sequence Distribution: </strong>During the whole process, the number of sequences, causes of errors and proportions of different types of errors change from time to time. Therefore, we counted and compared the numbers of DNA strands with errors and the left 100% correct DNA strands for each stage using a stacked column chart, as well as showed the changes in the strand numbers that contained different types of errors using line charts. Since the difference between the data is too large, each value y here is presented using ln(y).</li>
+                  <p></p>
+                  <p id='simudia-report'>From this chart, vertically, you could compare the ratio of modified strands to original ones and count of different error types for each stage. As for horizontally study, you could see that the total number of DNA strands, the ratio of strands with simulated errors to correct strands, and the number of simulated errors of different types vary with the stages.</p>
                   <Image src={simuseq} width={"75%"} rootClassName="image6"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 10: Example of sequence distribution diagram</p>
                   <li><strong>Error Counts: </strong>Because the effects of occurred errors are cumulative, it is reasonable that as the simulation proceeds, both the percentage of strands with errors as well as the average error number for all strands will increase. Thus, we count the number of strands with a different number of errors for the different stages as shown above. As we could see, the later the stage is, the higher the number of chains with errors is. Different colors here stand for different number of errors contained in the strands.</li>
                   <Image src={simucounts} width={"75%"} rootClassName="image6"/>
+                  <p style={{textAlign:"center",fontSize:13}}>Figure 11: Example of error counts diagram</p>
                   <li><strong>Download: </strong>After clicking on the download button, a zip folder containing simulated DNA sequences and an information YAML file will be downloaded automatically.</li>
                   <ul>
                     <li><strong>Simulated DNA Sequences: </strong>This file is in fasta format. The label of each sequence contains the error simulation results. For example, “{">"}11079[(77, 's', 'T'), (23, '+', 'C'), (36, '-', 'A')]” means, the 77th base, “T”,  of 11079th sequence has been substituted, 23rd position has inserted a 'C', and the 'A' on 36th has been deleted. </li>
